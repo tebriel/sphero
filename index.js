c6548e35425866d46ce4545061f4e8f25f3da7ea
Chris Moultrie <cmoultrie@endgame.com>
2014-01-15 19:54:24 -0700

    right port

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var Cylon = require('cylon');
 
 Cylon.robot({
-  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/rfcomm0' },
+  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/cu.Sphero-GOG-AMP-SPP' },
   device: {name: 'sphero', driver: 'sphero'},
 
-  work: function(my) {
-    every((1).second(), function() {
-      my.sphero.roll(60, Math.floor(Math.random() * 360));
-    });
-  }
+    work: function(my) {
+        my.sphero.stop()
+        every((1).second(), function() {
+            my.sphero.roll(60, Math.floor(Math.random() * 360));
+        });
+    }
 }).start();
